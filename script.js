4599fd8d0bd2bb0c32c8c19b80cf29ef823f83f0
ovhoffer <karilanko@wp.pl>
2022-06-28 23:28:04 +0200

    added the main block with adaptive for smartphones

---
+++ script.js
@@ -0,0 +1,18 @@
+// function changeImage(Classss)
+// {
+// 	element = document.querySelector(Classss).src ='images/CART.png'
+// 	return element
+	
+// }
+
+
+// function changeImagee(Classss)
+// {
+// 	element = document.querySelector(Classss).src = "images/cart-unactive-icon.svg"
+
+// 	return element
+	
+// }
+
+// // element = document.querySelector('.Cart-logo').href = "images\cart-unactive-icon.svg"
+// // 	console.log( document.querySelector('.Cart-logo'))
